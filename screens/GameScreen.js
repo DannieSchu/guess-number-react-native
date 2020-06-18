a9ddada3c5294f15c7c561a02109fe030ae948eb
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-17 23:31:13 -0700

    write function for generating a random number

---
+++ screens/GameScreen.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { generateRandomBetween } from '../utils/generateRandomBetween';
+
+const GameScreen = ({ userChoice }) => {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+});
+
+export default GameScreen;
